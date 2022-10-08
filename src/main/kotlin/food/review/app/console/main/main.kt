62fa724dd9f9f03ae1ad7c925056272d03cd2e72
AnthonyLonergan <lonergan921@gmail.com>
2022-10-08 17:22:01 +0100

    Very Basic Menu.

--- src/main/kotlin/food/review/app/console/main/main.kt
+++ src/main/kotlin/food/review/app/console/main/main.kt
@@ -1,5 +1,44 @@
 package food.review.app.console.main
 
+import mu.KotlinLogging
+import java.awt.SystemColor.menu
+
+private val logger = KotlinLogging.logger {}
+
 fun main(args: Array<String>) {
     println("Food Review Kotlin App by Anthony Lonergan")
+    logger.info { "Launching Food Review Console App" }
+
+    var input: Int
+
+    do {
+        input = menu()
+        when(input) {
+            1 -> println("You Chose Add Restaurant")
+            -1 -> println("Exiting App")
+            else -> println("Invalid Option")
+        }
+        println()
+    } while (input != -1)
+    logger.info { "Shutting Down Food Review Console App" }
+}
+
+fun menu() : Int {
+
+    var option : Int
+    var input: String? = null
+
+    println("Main Menu")
+    println(" 1. Add Restaurant Review")
+    println(" 2. Update Restaurant Review")
+    println(" 3. List All Previously Reviewed Restauraunts")
+    println("-1. Exit")
+    println()
+    print("Enter an integer : ")
+    input = readLine()!!
+    option = if (input.toIntOrNull() != null && !input.isEmpty())
+        input.toInt()
+    else
+        -9
+    return option
 }
